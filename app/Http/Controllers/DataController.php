e4d425a03dd6af7b74b829747ca121b0f52d4772
abhisanGhimire <abhisanghimire@dr-ait.org>
2020-12-28 12:33:19 +0545

    initial_commit

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Data;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    public function index()
+    {
+        $client       = new Client();
+        $request      = $client->get('https://portal.edcd.gov.np/rest/api/fetch?filter=casesBetween&type=dayByDay&sDate=2020-01-01&eDate=2020-12-24&disease=COVID-19', ['verify' => false]);
+        $response     = $request->getBody();
+        $decoded_info = json_decode($response);
+        Data::truncate();
+        foreach ($decoded_info as $all_info) {
+            $new_row = new Data;
+            if (isset($all_info->Province)) {
+                $new_row->province = $all_info->Province;
+            }
+            if (isset($all_info->District)) {
+
+                $new_row->district = $all_info->District;
+            }
+            if (isset($all_info->Sex)) {
+
+                $new_row->sex = $all_info->Sex;
+            }
+            if (isset($all_info->Age)) {
+                $new_row->age = $all_info->Age;
+            }
+            if (isset($all_info->Period)) {
+                $new_row->date = $all_info->Period;
+            }
+            if (isset($all_info->Value)) {
+                $new_row->value = $all_info->Value;
+            }
+            $new_row->save();
+
+        }
+		dd('Saved');
+    }
+
+    public function getUniquesvalueCount()
+    {
+        // $data = Data::pluck('district')->get();
+        $data = Data::select('district')->distinct()->get();
+        dd($data);
+    }
+}
---
+++ database/migrations/2020_12_25_075238_create_data_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('province')->nullable();
+            $table->string('sex')->nullable();
+            $table->string('age')->nullable();
+            $table->string('district')->nullable();
+            $table->string('date')->nullable();
+            $table->string('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+}
